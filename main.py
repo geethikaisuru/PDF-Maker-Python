6f5152eae4382e78d8493a72734faef4deef19ef
Geethika Isuru Sampath <35458073+geethikaisuru@users.noreply.github.com>
2024-12-01 10:50:21 +0530

    added pages accroding to topic pages

--- main.py
+++ main.py
@@ -12,5 +12,8 @@ for index, row in data.iterrows():
     pdf.set_font("Arial", size=12)
     pdf.cell(w=0, h=12, txt=row["Topic"], ln=1, align='L')
     pdf.line(10, 20, 200, 21)
+    for i in range(row["Pages"]):
+        pdf.add_page()
+        pdf.cell(w=0, h=12, txt=f"{row['Topic']} Page:{i+1}", ln=1, align='R')
 
 pdf.output('output.pdf')
